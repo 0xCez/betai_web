d272b342d9773aa3dfa327e42e243425d240ef29
Anas Alam <anasalam027@gmail.com>
2025-09-11 16:00:41 +0530

    Favicon added

--- app/routes/_layout.affiliation.tsx
+++ app/routes/_layout.affiliation.tsx
@@ -2,6 +2,14 @@ import CreatorOptions from "@/components/Affiliation/CreatorOptions";
 import FAQSection from "@/components/Affiliation/FAQSection";
 import TiktokCarousel from "@/components/Affiliation/TiktokCarousel";
 import VideoSection from "@/components/Affiliation/VideoSection";
+import type { MetaArgs } from "react-router";
+
+export function meta({}: MetaArgs) {
+    return [
+        { title: "Bet.AI" },
+        { name: "description", content: "Welcome to Bet.AI!" },
+    ];
+}
 
 export default function Affiliation() {
     return (
